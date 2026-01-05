1dc395441a9c5d31fb070862d69fa2687b40aea0
khanhtd8998 <trankhanhhy199x@gmail.com>
2026-01-05 23:01:31 +0700

    ini src

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), basicSsl()],
+  server: {
+    port: 3000
+  },
+  css: {
+    devSourcemap: true
+  },
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src')
+    }
+  }
+})
